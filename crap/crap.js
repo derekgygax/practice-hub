133d42f225be7adbef5f2d01d823372d910bc531
derekgygax <derekgygax@gmail.com>
2025-02-04 17:20:22 -0500

    a crap folder with things to mess about

---
+++ crap/crap.js
@@ -0,0 +1,5 @@
+console.log("HEY");
+
+for (i = 0; i < 5; i++) {
+  console.log(i);
+}
---
+++ crap/crap.ts
@@ -0,0 +1,16 @@
+console.log("hey");
+
+const i: string = "hey";
+
+console.log(i);
+
+function doSomething(): string {
+  console.log("do");
+
+  return "NO";
+}
+
+
+const bla: string = doSomething();
+
+console.log(bla);
