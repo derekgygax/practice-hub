13724889a1f48f77e851c8333d665f63052b55ae
derekgygax <derekgygax@gmail.com>
2025-03-20 08:22:00 -0400

    learning auth0 pulling info out of session

--- learning/Auth0_NextJS/auth0_v3/src/app/page.tsx
+++ learning/Auth0_NextJS/auth0_v3/src/app/page.tsx
@@ -1,15 +1,32 @@
+import { User } from "@/components/user/User";
 import styles from "./page.module.scss";
 
-export default function Home() {
+import { getSession, getAccessToken } from '@auth0/nextjs-auth0';
+
+export default async function Home() {
+
+  // Get the session
+  const session = await getSession();
+  console.log(session);
+  if (session) {
+    console.log(await getAccessToken() ?? "NO NON O");
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div>
-          <a href="/api/auth/login">Login</a>
-        </div>
-        <div>
-          <a href="/api/auth/logout">Logout</a>
-        </div>
+        {!session ? (
+          <div>
+            <a href="/api/auth/login">Login</a>
+          </div>
+        ) : (
+          <>
+            <div>
+              <a href="/api/auth/logout">Logout</a>
+            </div>
+            <User />
+          </>
+        )}
       </main>
     </div>
   );
--- learning/Auth0_NextJS/auth0_v3/src/components/user/User.tsx
+++ learning/Auth0_NextJS/auth0_v3/src/components/user/User.tsx
@@ -5,12 +5,21 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 export const User = () => {
   const { user, error, isLoading } = useUser();
 
+  // You  MUST perform this check for isLoading
+  // error and user BEFORE showing anything
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
+  // if (user) {
+  //   console.log(user);
+  // }
+
+  // You MUST check for user here!!
+  // There is sensitive info you may NOT want to show
   return (
     user && (
       <div>
+        <img src={user.picture ?? undefined} alt={user.name ?? undefined} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
       </div>
